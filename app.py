922037c9a539b950a2270ab09317b21b99e3af20
zuhorer <yashbhadauria@gmail.com>
2024-03-15 13:38:42 +0530

    added mixtral from aws bedrock

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 from mixtral_call import get_groq_response
+from aws_mixtral_call import get_bedrock_response
+from aws_mixtral_call_7b import get_bedrock_response_7b
 import json
 import time
 
@@ -40,14 +42,43 @@ def extract_json_from_string(string):
     
     return json_part
 
+def extract_nested_json(json_str):
+    try:
+        # Parse the string as JSON
+        outer_json = json.loads(json_str)
+        
+        # Access the 'text' field of the first element of the 'outputs' array
+        inner_json_str = outer_json['outputs'][0]['text']
+        
+        # Parse this string as JSON
+        inner_json = json.loads(inner_json_str)
+        
+        return inner_json
+    except json.decoder.JSONDecodeError as e:
+        print(f"Error parsing JSON: {e}")
+        print(f"JSON string: {json_str}")
+
 def call_api_2(keyword):
     results = get_groq_response(keyword)
-    try:
-        json_data = json.loads(results)
-    except ValueError:
-        # The JSON data is invalid
-        json_data = extract_json_from_string(results)
-    return json_data
+    # try:
+    #     json_data = json.loads(results)
+    # except ValueError:
+    #     # The JSON data is invalid
+    #     json_data = extract_json_from_string(results)
+    return results
+
+def call_api_3(keyword):
+    results = get_bedrock_response(keyword)
+    print("aws bedrock results",results.decode('utf-8'))
+   
+    return results.decode('utf-8')
+
+
+def call_api_4(keyword):
+    results = get_bedrock_response_7b(keyword)
+    print("aws bedrock results",results.decode('utf-8'))
+   
+    return results.decode('utf-8')
 
 def main():
     st.title("API Results Comparison")
@@ -56,7 +87,7 @@ def main():
     if st.button("Get Results"):
 
         if keyword:
-            col1, col2 = st.columns(2)
+            col1, col2, col3,col4 = st.columns(4)
             with col1:
                 st.write("Calling RNS...")
                 start_time = time.time()  # Start measuring time
@@ -68,14 +99,36 @@ def main():
             with col2:
                 st.write("Calling GROQ...")
                 start_time = time.time()  # Start measuring time
-                try:
-                    results_2 = json.loads(extract_json_from_string(call_api_2(keyword)))["output"]
-                except:
-                    results_2 = extract_json_from_string(call_api_2(keyword))                
+                # try:
+                results_2 = call_api_2(keyword)
+                # except:
+                #     results_2 = extract_json_from_string(call_api_2(keyword))                
                 end_time = time.time()  # Stop measuring time
                 response_time = end_time - start_time
                 st.write(f"Results from GROQ Mixtral received in {response_time} seconds:")
                 st.write(results_2)
+            with col3:
+                st.write("Calling AWS Sagemaker mixtral 8x7b...")
+                start_time = time.time()  # Start measuring time
+                # try:
+                results_3 = call_api_3(keyword)
+                # except:
+                #     results_2 = extract_nested_json(call_api_3(keyword))                
+                end_time = time.time()  # Stop measuring time
+                response_time = end_time - start_time
+                st.write(f"Results from AWS sagemaker Mixtral received in {response_time} seconds:")
+                st.write(results_3)
+            with col4:
+                st.write("Calling AWS Sagemaker mixtral 7b...")
+                start_time = time.time()  # Start measuring time
+                # try:
+                results_3 = call_api_3(keyword)
+                # except:
+                #     results_2 = extract_nested_json(call_api_3(keyword))                
+                end_time = time.time()  # Stop measuring time
+                response_time = end_time - start_time
+                st.write(f"Results from AWS sagemaker Mixtral received in {response_time} seconds:")
+                st.write(results_3)
         else:
             st.warning("Please enter a keyword")
 
---
+++ aws_mixtral_call.py
@@ -0,0 +1,213 @@
+
+import boto3
+import os
+import time
+import json
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings()
+
+db=FAISS.load_local('tlds.faiss', embeddings, allow_dangerous_deserialization=True)
+
+def create_bedrock_client():
+    client = boto3.client(service_name='bedrock-runtime')
+
+    # print(client.list_foundation_models())
+    return client
+
+client = create_bedrock_client()
+
+prompt_1 = """
+            You are a domain recommendation system.
+            Your objective is to recommend a domain name based on the user's input.
+            The domain names should be short, memorable, and relevant to the user's input.
+            
+            You can reshuffle, shorten, use synonyms, hyphens to the user's input to generate domain name suggestions.
+
+            The number of words in the domain name should be less than or equal to user query.
+
+            Only use the following TLDs for your suggestions if they make contextual sense else use generic TLDs like com, org, live: """
+prompt_2 = """
+            Here is an example of how to return your results in the following JSON format:
+            {
+            "output": {
+            "person-name": 0,
+            "domains": {
+            "count": 10,
+            "domains": [
+                {
+                "domain": "ApexSoftware.engineer",
+                "sld": "ApexSoftware",
+                "tld": "engineer",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 1.0,
+                "score": 1.0
+                },
+                {
+                "domain": "ApexSoftware.engineering",
+                "sld": "ApexSoftware",
+                "tld": "engineering",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 0.94,
+                "score": 0.94
+                },
+                {
+                "domain": "ApexSoftware.download",
+                "sld": "ApexSoftware",
+                "tld": "download",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 0.84,
+                "score": 0.84
+                },
+                {
+                "domain": "ApexSoftware.tech",
+                "sld": "ApexSoftware",
+                "tld": "tech",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 0.65,
+                "score": 0.65
+                },
+                {
+                "domain": "ApexSoftware.technology",
+                "sld": "ApexSoftware",
+                "tld": "technology",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 0.64,
+                "score": 0.64
+                '},
+                '{
+                "domain": "ApexSoftware.reviews",
+                "sld": "ApexSoftware",
+                "tld": "reviews",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 0.56,
+                "score": 0.56
+                },
+                {
+                "domain": "Apex-Software.engineer",
+                "sld": "Apex-Software",
+                "tld": "engineer",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 0.5,
+                "tldValue": 1.0,
+                "score": 0.5
+                },
+                {
+                "domain": "Apex-Software.engineering",
+                "sld": "Apex-Software",
+                "tld": "engineering",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 0.5,
+                "tldValue": 0.94,
+                "score": 0.47
+                },
+                {
+                "domain": "Apex-Software.download",
+                "sld": "Apex-Software",
+                "tld": "download",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 0.5,
+                "tldValue": 0.84,
+                "score": 0.42
+                },
+                {
+                "domain": "ApexSoftware.computer",
+                "sld": "ApexSoftware",
+                "tld": "computer",
+                "tldPhase": "GA",
+                "exact": 0,
+                "registered": 0,
+                "premium": 0,
+                "adult": 0,
+                "sldValue": 1.0,
+                "tldValue": 0.42,
+                "score": 0.42
+                }
+            ]
+            }
+            }
+            }
+
+            Only return the JSON in proper format and nothing else.
+            """
+
+def prompt_builder(keyword):
+    tlds = [d.page_content for d in db.similarity_search(keyword, k=5)]
+    system_prompt = prompt_1 + ", ".join(tlds) +"\n\n"+ prompt_2
+    # print(system_prompt)
+    system= "<s>[INST] " + system_prompt + " [INST]</s> "
+    user = "<s>[USER] " + keyword + " [USER]</s>"
+    return system  + user
+
+
+def get_bedrock_response(keyword):
+    prompt = prompt_builder(keyword)
+
+    body = json.dumps({
+        "prompt": prompt,
+        # "max_tokens": 4160,
+        "temperature": 0,
+        "top_p": 1,
+        "top_k": 50
+    })
+    # body = json.dumps({
+    #     "prompt": "\n\nHuman:explain black holes to 8th graders\n\nAssistant:",
+    #     # "max_tokens_to_sample": 300,
+    #     "temperature": 0.1, 
+    #     "top_p": 0.9,
+    #     })
+
+
+    completion = client.invoke_model(
+        body= body, 
+        modelId="mistral.mixtral-8x7b-instruct-v0:1", 
+        accept = "application/json",
+        contentType = "application/json"
+    )
+    
+    body_content = completion.get('body').read()
+    # print("response body content",body_content,type(body_content))
+    # return json.loads(body_content
+    return  body_content
